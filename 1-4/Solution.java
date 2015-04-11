5b92bbd41b410399965584a9436d07824961ec1b
Eric Turner <ericturnerdev@gmail.com>
2015-04-12 00:57:36 +0900

    Add solution 1-4

--- 1-4/Solution.java
+++ 1-4/Solution.java
@@ -4,50 +4,65 @@ public class Solution {
   public static void main(String args[]) {
     final long startTime = System.nanoTime();
 
-    /*
-    String testString1 = "apples";
-    String testString2 = "ppseal";
-    String testString4 = "bananas";
-    String testString5 = "appleX";
-    String testString6 = null;
-    String testString7 = "not null";
-    String testString8 = "";
-    String testString9 = "   a";
-    String testString10 = "  a ";
-    String testString11 = "a  ";
+    String testString1 = "apples   ";
+    String testString2 = "   apples      ";
+    String testString3 = "ba na na s         ";
+    String testString4 = "   ";
     System.out.println("---Begin test---");
 
-    System.out.println("For " + testString1 + " and " + testString2 + ": ");
-    System.out.println(isPermutation(testString1, testString2));
+    System.out.println("For \"" + testString1 + "\": ");
+    System.out.println(replaceSpaces(testString1.toCharArray(), 7));
 
-    System.out.println("For " + testString1 + " and " + testString4 + ": ");
-    System.out.println(isPermutation(testString1, testString4));
+    System.out.println("For \"" + testString2 + "\": ");
+    System.out.println(replaceSpaces(testString2.toCharArray(), 9));
 
-    System.out.println("For " + testString1 + " and " + testString5 + ": ");
-    System.out.println(isPermutation(testString1, testString5));
+    System.out.println("For \"" + testString3 + "\": ");
+    System.out.println(replaceSpaces(testString3.toCharArray(), 11));
 
-    System.out.println("For " + testString6 + " and " + testString6 + ": ");
-    System.out.println(isPermutation(testString6, testString6));
-
-    System.out.println("For " + testString6 + " and " + testString7 + ": ");
-    System.out.println(isPermutation(testString6, testString7));
-
-    System.out.println("For " + testString7 + " and " + testString8 + ": ");
-    System.out.println(isPermutation(testString7, testString8));
-
-    System.out.println("For " + testString8 + " and " + testString8 + ": ");
-    System.out.println(isPermutation(testString8, testString8));
-
-    System.out.println("For " + testString9 + " and " + testString10 + ": ");
-    System.out.println(isPermutation(testString9, testString10));
-
-    System.out.println("For " + testString10 + " and " + testString11 + ": ");
-    System.out.println(isPermutation(testString10, testString11));
-    */
+    System.out.println("For \"" + testString4 + "\": ");
+    System.out.println(replaceSpaces(testString4.toCharArray(), 1));
 
     /* Print total time spent */
     final long duration = System.nanoTime() - startTime;
     System.out.println("\nduration: " + duration/1000000000.0 + "s");
+  }
+
+  public static char[] replaceSpaces(char[] chars, int length) {
+    if (chars == null) { return null; }
+    if (length == 0) { return chars; }
+    int spaceCount = 0;
+    for(int i=0; i<length; i++) {
+      if (chars[i] == ' ') {
+        spaceCount++;
+      }
+    }
+    int shortPos = length-1;
+    int longPos = shortPos+(spaceCount*2);
+    for (int i=0; i<length; i++) {
+      if (chars[shortPos] == ' ') {
+        chars[longPos] = '0';
+        chars[longPos-1] = '2';
+        chars[longPos-2] = '%';
+        longPos -= 3;
+        shortPos--;
+      }
+      else {
+        chars[longPos] = chars[shortPos];
+        shortPos--;
+        longPos--;
+      }
+    }
+    System.out.println("final:");
+    printCharArray(chars);
+    return chars;
+  }
+
+  public static void printCharArray(char[] arr) {
+    System.out.print("[");
+    for(char c : arr) {
+      System.out.print(c + " ");
+    }
+    System.out.print("]\n");
   }
 
 }
