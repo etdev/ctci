42fe688d22f74d8481056c2531c7bc128d9fa60f
Eric Turner <ericturnerdev@gmail.com>
2015-04-12 17:53:32 +0900

    Slightly more progress on 1-6

--- 1-6/Solution.java
+++ 1-6/Solution.java
@@ -10,6 +10,15 @@ public class Solution {
     char[] testArray2 = { 'd', 'e', 'f' };
     char[] testArray3 = { 'g', 'h', 'i' };
     char[][] testMatrix1 = { testArray1, testArray2, testArray3 };
+    System.out.println("---testMatrix1---");
+    printCharMatrix(testMatrix1);
+    System.out.println("[0][0]");
+    System.out.println(testMatrix1[0][0]);
+    System.out.println("[2][2]");
+    System.out.println(testMatrix1[2][2]);
+    System.out.println("---result---");
+    testMatrix1 = rotateMatrix(testMatrix1);
+    printCharMatrix(testMatrix1);
 /*
     //System.out.println("For \"" + testString1 + "\": ");
     System.out.println(compressString(testString1));
@@ -30,15 +39,21 @@ public class Solution {
 
   public static char[][] rotateMatrix(char[][] matrix) {
     if (matrix == null) { return null; }
-    if (matrix.length == 0 || matrix[0].length == 0 || matrix.length == matrix[0].length) {
+    System.out.println("matrix.length: " + matrix.length);
+    System.out.println("matrix[0].length: " + matrix[0].length);
+    if (matrix.length == 0 || matrix[0].length == 0 || matrix.length != matrix[0].length) {
       return matrix;
     }
     char temp = '\0';
     int offY = 0;
     int offX = 0;
     for (int i=0; i<matrix.length/2; i++) {
-      for (int j=0; j<matrix.length; i++) {
+      for (int j=0; j<matrix.length; j++) {
+        System.out.println("i: " + i + ", j: " + j);
         temp = matrix[i][j];
+        System.out.println("temp: " + temp);
+        offY = Math.abs(matrix.length-j);
+        System.out.println("offY: " + offY);
         offY = Math.abs(matrix.length-j);
         offX = Math.abs(matrix.length-i);
         matrix[i][j] = matrix[offY][i];
@@ -54,5 +69,11 @@ public class Solution {
       System.out.print(c + " ");
     }
     System.out.print("]\n");
+  }
+
+  public static void printCharMatrix(char[][] matrix) {
+    for (char[] c: matrix) {
+      printCharArray(c);
+    }
   }
 }
